de5a8baa92941a88ef69aab781ed0d57bca9317c
allanleandrof <alanleandrof@gmail.com>
2025-02-27 23:33:33 -0300

    feat: Environment configuration

---
+++ src/main/java/com/example/management_wallet_api/ManagementWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.management_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementWalletApiApplication.class, args);
+	}
+
+}
